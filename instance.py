83c07138e23f7705f74326dbd4e17eba8b7b1d9f
Jan Svrcina <jan.svrcina@siemens.com>
2025-11-01 18:39:20 +0100

    instance data

---
+++ instance.py
@@ -0,0 +1,112 @@
+import sys
+import json
+
+from dataclasses import dataclass, field
+from typing import List, Dict
+
+DEFAULT_DATA = 'data/nor1_critical_0.json'
+MAX_DUR = 100000
+
+@dataclass
+class Res:
+	idx: int = -1
+	time: int = 0
+
+
+@dataclass
+class Obj:
+	threshold: int = 0
+	coeff: int = 0
+	increment: int = 0
+
+
+@dataclass
+class Op:
+	idx: int = -1
+	train: int = -1
+
+	dur: int = 0
+	start_lb: int = 0
+	start_ub: int = MAX_DUR
+
+	succ: List[int] = field(default_factory=list)
+	prev: List[int] = field(default_factory=list)
+
+	res: List[Res] = field(default_factory=list)
+
+	obj: Obj|None = None	
+
+
+@dataclass
+class Point:
+	idx: int = -1
+	train: int = -1
+
+
+@dataclass
+class Train:
+	idx: int = -1
+
+	ops: List[Op] = field(default_factory=list)
+	points: List[Point] = field(default_factory=list)
+
+
+class Instance:
+	trains: List[Train]
+	__res_name_idx: Dict[str, int]
+
+	def __init__(self, jsn_file: str):
+
+		self.parse_json_file(jsn_file)
+
+
+	def parse_json_file(self, jsn_file: str):
+		self.__res_name_idx = {}
+
+		with open(jsn_file, 'r') as fd:
+			jsn = json.load(fd)
+		
+		for jsn_train in jsn['trains']:
+			self.parse_json_train(jsn_train)
+
+
+	def parse_json_train(self, jsn_train: dict):
+		train = Train(idx=len(self.trains))
+		
+		for jsn_op in jsn_train:
+			self.parse_json_op(jsn_op, train)
+
+
+	def parse_json_op(self, jsn_op: dict, train: Train):
+		op = Op(idx=len(train.ops), train=train.idx)
+
+		op.dur = jsn_op['dur']
+		op.start_lb = jsn_op.get('start_lb', 0)
+		op.start_ub = jsn_op.get('start_ub', MAX_DUR)
+
+		op.succ = jsn_op['successors']
+
+		for jsn_res in jsn_op.get('resources', []):
+			res = Res(
+				idx=self.get_res_idx(jsn_res['resource']),
+				time=jsn_res['release_time'])
+			
+			op.res.append(res)
+
+		train.ops.append(op)
+
+	
+	def get_res_idx(self, name):
+		idx = self.__res_name_idx.get(name, -1)
+		
+		if idx == -1:
+			idx = len(self.__res_name_idx)
+			self.__res_name_idx[name] = idx
+
+		return idx
+
+if __name__ == '__main__':
+	data = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_DATA
+	print(data)
+	inst = Instance(data)
+	pass
